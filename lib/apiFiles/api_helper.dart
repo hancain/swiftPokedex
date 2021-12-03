37580065c9876223c1f15f5490acf14c6ffe143a
Hannah Cain <hcsch18@gmail.com>
2021-12-03 12:29:25 -0500

    working api calls

--- lib/apiFiles/api_helper.dart
+++ lib/apiFiles/api_helper.dart
@@ -1,33 +1,23 @@
-import 'dart:io';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'dart:async';
+import 'dart:io';
+import 'package:pokedex/apiFiles/pokemon_object.dart';
 
+const maxNum = 151;
+String url = 'https://pokeapi.co/api/v2/pokemon/';
 
-class ApiBaseHelper {
-  final String _mainURL = "https://pokeapi.co/api/v2/";
-
-  Future<dynamic> get(String url) async {
-    dynamic responseObj;
-    try {
-      final response = await http.get(Uri.parse(_mainURL + url));
-      responseObj = _returnResponse(response);
-    }
-    on SocketException {
-      throw Exception('No connection');
-    }
-    return responseObj;
+Future<List<Pokemon>> getAllPokemon() async {
+  var response;
+  List<Pokemon> pokemonList = [];
+  for(int i = 1; i <= maxNum; i++) {
+    response = await http.get(Uri.parse(url + i.toString()));
+    pokemonList.add(pokemonFromJson(response.body));
   }
-
-  dynamic _returnResponse(http.Response response) {
-    switch (response.statusCode) {
-      case 200:
-        var responseJson = json.decode(response.body.toString());
-        return responseJson;
-      default:
-        throw Exception(
-            'Error occured while Communication with Server with StatusCode : ${response
-                .statusCode}');
-    }
-  }
+  return pokemonList;
 }
+
+Future<Pokemon> getPokemon(String i) async{
+  final response = await http.get(Uri.parse(url + i));
+  return pokemonFromJson(response.body);
+}
+
--- lib/apiFiles/pokemon_object.dart
+++ lib/apiFiles/pokemon_object.dart
@@ -1,26 +1,63 @@
+import 'dart:convert';
+
+Pokemon pokemonFromJson(String str){
+    final jsonData = json.decode(str);
+    return Pokemon.fromJson(jsonData);
+}
+
+List<Pokemon> allPokemonFromJson(String str) {
+    final jsonData = json.decode(str);
+    return List<Pokemon>.from(jsonData.map((x) => Pokemon.fromJson(x)));
+}
+
 class Pokemon {
     int id;
     String name;
-    String sprite;
-    List<String> types;
+    Sprite sprite;
+    //List<Type> types;
 
-    Pokemon({required this.id, required this.name, required this.sprite, required this.types});
+    Pokemon({
+        required this.id,
+        required this.name,
+        required this.sprite,
+        //required this.types,
+    });
 
-    factory Pokemon.fromJson(Map<String, dynamic> json) {
-        return Pokemon(
-            id: json['id'],
-            name: json['name'],
-            sprite: json['sprite'],
-            types: json['types'],
-        );
-    }
+    factory Pokemon.fromJson(Map<String, dynamic> json) => Pokemon(
+        id: json["id"],
+        name: json["name"],
+        sprite: Sprite.fromJson(json["sprites"]),
+        //types: json["types"].map((type) => Type.fromJson(json["type"])).toList()
+    );
 
-    Map<String, dynamic> toJson() {
-        final Map<String, dynamic> data = <String, dynamic>{};
-        data['id'] = id;
-        data['name'] = name;
-        data['sprites'] = sprite;
-        data['types'] = types.map((v) => v).toList();
-        return data;
-    }
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "sprite": sprite,
+        //"types": types
+    };
+}
+
+class Type{
+    String name;
+    
+    Type({
+        required this.name,
+    });
+    
+    factory Type.fromJson(Map<String, dynamic> json) => Type(
+        name: json["name"]
+    );
+}
+
+class Sprite{
+    String front;
+
+    Sprite({
+        required this.front
+    });
+
+    factory Sprite.fromJson(Map<String, dynamic> json) => Sprite(
+        front: json["front_default"]
+    );
 }
